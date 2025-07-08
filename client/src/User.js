333dc05b06678041c8f0f4dbe046e35025baab40
Suniil Ghate <sunil.82ghate@gmail.com>
2025-07-08 18:30:54 +0530

    socket url changed

--- client/src/User.js
+++ client/src/User.js
@@ -8,7 +8,7 @@ function User() {
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        const newSocket = new WebSocket('ws://https://chat-app-agentic.onrender.com');
+        const newSocket = new WebSocket('ws://chat-app-agentic.onrender.com');
         setSocket(newSocket);
 
         newSocket.onopen = () => {
